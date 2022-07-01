bf58fd2faf90f45ae7745f0f4e896e95c21f143b
Michał Wójcik <01153102@pw.edu.pl>
2022-07-01 17:39:49 +0200

    xd

--- Secure-bank-api.Auth/src/main/java/pl/edu/pw/security/config/CustomAuthenticationProvider.java
+++ Secure-bank-api.Auth/src/main/java/pl/edu/pw/security/config/CustomAuthenticationProvider.java
@@ -1,21 +1,45 @@
 package pl.edu.pw.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import pl.edu.pw.repository.AccountRepository;
+import pl.edu.pw.user.Account;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final AccountRepository accountRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
-
+        Optional<Account> account = accountRepository.findByAccountNumber(name);
+        if (account.isPresent()) {
+//            int passwordHashId = Session.get("hashId");
+//            Hash hash = hashRepository.getHash(passwordHashId);
+            if (bCryptPasswordEncoder.matches(password, hash)) {
+                return new UsernamePasswordAuthenticationToken(
+                        name, password, new ArrayList<>());
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
